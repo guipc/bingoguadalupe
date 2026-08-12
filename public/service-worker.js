e92d5b8c3540fec8ae0a61c2237ba9a37e01ba05
guipc <45241999+guipc@users.noreply.github.com>
2026-08-12 14:09:16 +0100

    Init dev

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "bingo-cache-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/logofesta.png",
+                "/igreja.png",
+                "/manifest.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => {
+            return resp || fetch(event.request);
+        })
+    );
+});
